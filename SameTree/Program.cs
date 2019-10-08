7536d22fb8c68deda0a3ba5be0cb194304e95730
Liquid Falme <sansari1@cox.net>
2019-10-08 10:27:22 -0400

    Successfully submitted to LeetCode. Did not bother to test it in Visual Studio. This one is pretty straight forward.

---
+++ SameTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SameTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        private static Boolean IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p==null && q == null)
+            {
+                return true;
+            }
+            if (p != null && q != null)
+            {
+                if (p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
